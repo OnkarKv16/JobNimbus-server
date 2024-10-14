7a45b46ddc1da7c216ddb00056f7367f794ec94a
Onkar Vallal <okvallal200@gmail.com>
2024-10-14 23:00:09 +0530

    commit

---
+++ src/main/java/com/jobportal/dto/ProfileDTO.java
@@ -0,0 +1,30 @@
+package com.jobportal.dto;
+
+import java.util.Base64;
+import java.util.List;
+
+import com.jobportal.entity.Profile;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProfileDTO {
+	private long id;
+	private String email;
+	private String jobTitle;
+	private String company;
+	private String location;
+	private String about;
+	private String picture;
+	private List<String> skills;
+	private List<Experience> experiences;
+	private List<Certification> certifications;
+	
+	public Profile toEntity() {
+		return new Profile(this.id, this.email, this.jobTitle, this.company, this.location, this.about,this.picture!=null?Base64.getDecoder().decode(this.picture):null,this.skills, this.experiences, this.certifications);
+	}
+}
